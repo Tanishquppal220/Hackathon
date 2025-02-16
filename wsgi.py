69425f5bdb06334c0afea6cb6dc479ca2ea7f90a
Tanishq Uppal <tanishquppal220@gmail.com>
2025-02-16 20:24:24 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,13 @@
-from Hackathon.app import app
+import sys
+import os
 
-application = app
+# Add your project directory to the sys.path
+project_home = '/home/Tanishquppal2200/Hackathon'
+if project_home not in sys.path:
+    sys.path.append(project_home)
+
+# Set the environment variable for the Flask app
+os.environ['FLASK_APP'] = 'app.py'
+
+# Import the Flask app
+from app import app as application
